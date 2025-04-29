50053ebcdec21e00479a83aecfc64bc497b59bb8
Lasith Amanda <lasithamanda1999@example.com>
2025-04-29 17:24:20 +0530

    Project Setup and add Shadcn

---
+++ sommaire/app/layout.tsx
@@ -0,0 +1,32 @@
+import type { Metadata } from "next";
+import { Source_Sans_3 as FontSans} from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  variable: "--font-sans",
+  subsets: ["latin"],
+  weight: ['200', '300', '400', '500', '600', '700', '800', '900'],
+
+});
+
+
+export const metadata: Metadata = {
+  title: "Sommaire - AI-Powered PDF Summerization",
+  description: "Save hours of reading time. Transform lengthy PDFs into clear, accurate summaries in seconds with our advanced AI technology.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${fontSans.variable} font-sans antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
